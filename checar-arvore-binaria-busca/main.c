ef2ea87f88248089e2a48e7dac110d1e78545586
Robson Mariano <robson.mariano@mirumagency.com>
2022-04-03 21:02:51 -0300

    adiciona main arvore binaria de busca

---
+++ checar-arvore-binaria-busca/main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Robson Mariano - GRR20204458
+
+typedef struct arvore {
+    int info;
+    struct arvore *esq;
+    struct arvore *dir;
+} Arvore;
+
+int buscar (Arvore *a, int v) {
+    if (a == NULL) { return 0; } /*Nao achou*/
+    else if (v < a->info) {
+        return buscar (a->esq, v);
+    }
+    else if (v > a->info) {
+        return buscar (a->dir, v);
+    }
+    else { return 1; } /*Achou*/
+}
+
+Arvore* inserir (Arvore *a, int v) {
+    if (a == NULL) {
+        a = (Arvore*)malloc(sizeof(Arvore));
+        a->info = v;
+        a->esq = a->dir = NULL;
+    }
+    else if (v < a->info) {
+        a->esq = inserir (a->esq, v);
+    }
+    else { a->dir = inserir (a->dir, v); }
+    return a;
+}
+
+void in_order(Arvore *a){
+    if(!a)
+        return;
+    in_order(a->esq);
+    printf("%d ",a->info);
+    in_order(a->dir);
+}
+
+Arvore* remover(Arvore *a, int x){
+    Arvore * aux, * pai_aux;
+    int filhos = 0,tmp;
+
+    if(!a)
+        return(NULL);
+
+    if(a->info < x)
+        a->dir = remover(a->dir,x);
+    else if(a->info > x)
+        a->esq = remover(a->esq,x);
+    else{
+        if(a->esq)
+            filhos++;
+        if(a->dir)
+            filhos++;
+
+        if(filhos == 0){
+            free(a);
+            return(NULL);
+        }
+        else if(filhos == 1){
+            aux = a->esq ? a->esq : a->dir;
+            free(a);
+            return(aux);
+        }
+        else{
+            aux = a->esq;
+            pai_aux = a;
+            while(aux->dir){ pai_aux = aux; aux = aux->dir; }
+            tmp = a->info;
+            a->info = aux->info;
+            aux->info = tmp;
+            pai_aux->dir = remover(aux,tmp);
+            return(a);
+        }
+    }
+
+    return(a);
+}
+
+void print(Arvore * a,int spaces){
+    int i;
+    for(i=0;i<spaces;i++) printf(" ");
+    if(!a){
+        printf("//\n");
+        return;
+    }
+
+    printf("%d\n", a->info);
+    print(a->esq,spaces+2);
+    print(a->dir,spaces+2);
+}
+
+
+int isBST(Arvore *a) {
+    if (a == NULL){
+        return 1;
+    }
+
+    /* falso se a esq eh maior que o node */
+    if (a->esq != NULL && a->esq->info > a->info){
+        return 0;
+    }
+
+    /* falso se a dir eh menor que o node */
+    if (a->dir != NULL && a->dir->info < a->info){
+        return 0;
+    }
+
+    /* falso se recursivamente, a esq ou dir nao eh uma arvore binaria de busca */
+    if (!isBST(a->esq) || !isBST(a->dir)){
+        return 0;
+    }
+
+    /* Se passar tudo, é uma arvore binaria de busca */
+    return 1;
+
+}
+
+int main(){
+    Arvore * a;
+
+    a = inserir(NULL,50);
+    a = inserir(a,30);
+    a = inserir(a,90);
+    a = inserir(a,20);
+    a = inserir(a,40);
+    a = inserir(a,95);
+    a = inserir(a,10);
+    a = inserir(a,35);
+    a = inserir(a,45);
+    a = inserir(a,37);
+
+    printf("\n");
+    print(a,0);
+    printf("\n");
+
+    a = remover(a,45);
+    a = remover(a,50);
+    a = remover(a,90);
+
+    printf("\n");
+    print(a,0);
+    printf("\n");
+
+    int final = isBST(a);
+
+    if(final==1){
+        printf("Eh uma arvore binaria de busca\n");
+    }
+    if(final==0){
+        printf("Nao eh uma arvore binaria de busca\n");
+    }
+}
